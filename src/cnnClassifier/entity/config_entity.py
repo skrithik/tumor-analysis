f227ac109aa7aa3ddb2d01f91a237a83ebb94206
skrithik <saiskrithik.k@gmail.com>
2025-07-05 13:34:39 +0530

    training done

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+#entity
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+#entity
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+#entity
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
